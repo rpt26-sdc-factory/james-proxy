aebcfeb2eeec4dd8369a91fa02d394860a495b67
James Henderson <jhende1995@gmail.com>
2021-06-14 15:35:42 -0400

    implemented server side rendering
    Currently on renders my service. I'll add the rest in when i have their
    urls

--- server/index.js
+++ server/index.js
@@ -1,21 +1,64 @@
+const { promisify } = require('util');
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+var read = promisify(fs.readFile);
 const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3000;
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+const axios = require('axios');
+
+require('dotenv').config();
+
+
+var urls = {
+  About: process.env.ABOUTURL || 'http://localhost:3002'
 }
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.get('/:id', (req, res, next) => {
+  if (isNaN(req.params.id)) {
+    next();
+    return;
+  }
+  read(`${__dirname}/../public/index.html`, {encoding: 'utf8'} ).then(html => {
+    var renderPromises = [
+      axios.get(`${urls.About}/${req.params.id}/innerHTML`).then((results) => {
+        html = html.replace(`<div id="about"></div>`, `
+          <div id="about">${results.data}</div>
+        `);
+      })
+    ];
+
+    Promise.all(renderPromises).then(() => {
+      res.send(html);
+    })
+  })
+});
+
+app.get('/index.js', (req, res) => {
+  var renderPromises = [
+    axios.get(`${urls.About}/index.js`).then((results) => {
+      return `(() => {${results.data}})()`;
+    }).catch(() => {
+      console.log('failed to get js from about service');
+    })
+  ];
+
+  Promise.all(renderPromises).then(jsArr => {
+    return jsArr.join('\n');
+  }).then(js => {
+    res.status(200).send(js);
+  })
+})
+
 app.use(express.static(path.resolve(__dirname, '../public')));
 
-app.get('/:id', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
-});
+
 
 app.listen(PORT, () => {
   console.log(`Proxy listening at port ${PORT}`);
