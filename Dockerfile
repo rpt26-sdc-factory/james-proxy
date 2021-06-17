caa0e52a54e02c1bf8e714688baef4aaa3ec3be4
James Henderson <jhende1995@gmail.com>
2021-06-17 13:02:26 -0400

    ready for production deploy

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
-FROM node:14
-ENV NODE_ENV=production
-WORKDIR /usr/src/app
-COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
-RUN npm install --production --silent && mv node_modules ../
+FROM node:15.12.0-alpine3.13
+WORKDIR /app
+COPY package*.json ./
+RUN npm install
 COPY . .
 EXPOSE 3000
-CMD ["npm", "start"]
+CMD [ "npm", "start" ]
--- server/index.js
+++ server/index.js
@@ -24,19 +24,33 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const renderFunctions = {
-  HTML: (url = '', productId = 1, divId = '') => {
-    return axios.get(`${url}/${productId}/innerHTML`).then((results) => {
-      html = html.replace(`<div id="${divId}"></div>`, `
-        <div id="${divId}">${results.data}</div>
-      `);
-    })
+  HTML: (url = '', productId = 1, name) => {
+    return axios.get(`${url}/${productId}/innerHTML`)
+    .then(
+      (results) => {
+        return {
+          name,
+          html: results.data
+        };
+      }, (error) => {
+        console.log('failed to get html from service at', url);
+      }
+    );
   },
   JS: (url, bundleName) => {
     return axios.get(`${url}/${bundleName}.js`).then((results) => {
-      return `(() => {${results.data}})()`;
-    }).catch(() => {
-      console.log('failed to get js from about service');
+      return renderFunctions.arrowWrapper(results.data);
+    }).catch((err) => {
+      console.log('failed to get js from service at', url);
     })
+  },
+  arrowWrapper: (str) => {
+    return typeof str !== 'string' && typeof str !== undefined ?
+      `(() => {${str.toString()}})();` :
+      `(() => {${str}})();` ;
+  },
+  divReplacer: (html, data, id) => {
+    return html.replace(`<div id="${id}"></div>`, `<div id="${id}">${data}</div>`);
   }
 }
 
@@ -45,15 +59,24 @@ app.get('/:id', (req, res, next) => {
     next();
     return;
   }
+  console.log(req.params.id);
   read(`${__dirname}/../public/index.html`, {encoding: 'utf8'} ).then(html => {
+    html = html.replace('<meta http-equiv="refresh" content="0; URL=/1" />','');
     var renderPromises = [
       renderFunctions.HTML(urls.about, req.params.id, 'about'),
-      renderFunctions.HTML(urls.title, req.params.id, 'about'),
-      renderFunctions.HTML(urls.instructors, req.params.id, 'about'),
-      renderFunctions.HTML(urls.syllabus, req.params.id, 'about'),
+      //renderFunctions.HTML(urls.title, req.params.id, 'title'),
+      renderFunctions.HTML(urls.instructors, req.params.id, 'instructors'),
+      renderFunctions.HTML(urls.syllabus, req.params.id, 'syllabus'),
     ];
 
-    Promise.all(renderPromises).then(() => {
+    Promise.allSettled(renderPromises).then((promises) => {
+      var datas = [];
+      promises.forEach((promise) => {
+        promise.value ? datas.push(promise.value) : null;
+      });
+      html = datas.reduce((html, curData) => {
+        return renderFunctions.divReplacer(html, curData.html, curData.name);
+      }, html);
       res.send(html);
     })
   })
@@ -61,15 +84,19 @@ app.get('/:id', (req, res, next) => {
 
 app.get('/index.js', (req, res) => {
   var renderPromises = [
-    read(`${__dirname}/../public/index.js`),
+    read(`${__dirname}/../public/index.js`, {encoding: "utf-8"}).then(renderFunctions.arrowWrapper),
     renderFunctions.JS(urls.about, 'index'),
-    renderFunctions.JS(urls.title, 'bundle'),
-    renderFunctions.JS(urls.instructors, 'bundle'),
+    //renderFunctions.JS(urls.title, 'bundle'), //incompatible with my ssr
+    //renderFunctions.JS(urls.instructors, 'bundle'), //error in code
     renderFunctions.JS(urls.syllabus, 'bundle')
   ];
 
-  Promise.all(renderPromises).then(jsArr => {
-    return jsArr.join('\n');
+  Promise.allSettled(renderPromises).then(promises => {
+    let datas = [];
+    promises.forEach(promise => {
+      promise.value !== undefined ? datas.push(promise.value.toString()) : null;
+    });
+    return datas.join('\n//end of JS Source \n');
   }).then(js => {
     res.status(200).send(js);
   })
